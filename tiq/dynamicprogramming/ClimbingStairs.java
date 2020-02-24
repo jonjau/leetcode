703ede5794f4092819c3d6f6908f3cc2150a9090
jonjau <jonjau3Q@gmail.com>
2020-02-24 19:36:46 +1100

    climbing stairs (java): recursive with memoization

--- tiq/dynamicprogramming/ClimbingStairs.java
+++ tiq/dynamicprogramming/ClimbingStairs.java
@@ -26,8 +26,8 @@ package tiq.dynamicprogramming;
  */
 public class ClimbingStairs {
     public static int run() {
-        int n = 4;
-        int ans = climbStairs1(n);
+        int n = 44;
+        int ans = climbStairs2(n);
         System.out.println(ans);
         return ans;
     }
@@ -65,5 +65,45 @@ public class ClimbingStairs {
         }
         // recursive case, i still less than n, so try to fit in more (with either 1 or 2 steps)
         return climbStairs1helper(i + 1, n) + climbStairs1helper(i + 2, n);
+    }
+
+    /**
+     * Recursive, with memoization: find the number of distinct ways to
+     * get to exactly n steps with steps of 1 and/or 2
+     * <p>
+     *     O(n) time (size of recursion tree can go up to n)
+     *     O(n) space (depth of recursion tree can go up to n)
+     * </p>
+     *
+     * @param n the positive integer number of steps to get to
+     * @return the number of distinct ways to get to n, with steps of 1 and/or 2
+     */
+    public static int climbStairs2(int n) {
+        int[] memo = new int[n + 1];
+        return climbStairs2helper(0, n, memo);
+    }
+
+    /**
+     * Recursive helper function for climbStairs2
+     *
+     * @param i current step
+     * @param n destination step
+     * @param memo integer array storing the number of distinct ways to get to each i
+     * @return number of distinct ways to arrange steps of 1 and/or 2 to get to n, starting from i
+     */
+    public static int climbStairs2helper(int i, int n, int[] memo) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        // number of distinct ways to get to the i'th step has been calculated before
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+        // never been to this i before, calculate and memoize
+        memo[i] = climbStairs2helper(i + 1, n, memo) + climbStairs2helper(i + 2, n, memo);
+        return memo[i];
     }
 }
