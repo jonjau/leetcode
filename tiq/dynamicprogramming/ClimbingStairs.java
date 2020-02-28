ae994ecbef8dcc73a0bdb895b86dfac84e34a4b2
jonjau <jonjau3Q@gmail.com>
2020-02-28 20:24:59 +1100

    climbing stairs (java): dynamic programming

--- tiq/dynamicprogramming/ClimbingStairs.java
+++ tiq/dynamicprogramming/ClimbingStairs.java
@@ -36,8 +36,8 @@ public class ClimbingStairs {
      * VERY SLOW (TIME LIMIT EXCEEDED) Recursive brute force: find the number of distinct ways to
      * get to exactly n steps with steps of 1 and/or 2
      * <p>
-     *     O(2^n) time (size of recursion tree will be 2^n)
-     *     O(n) space (depth of recursion tree )
+     * O(2^n) time (size of recursion tree will be 2^n)
+     * O(n) space (depth of recursion tree )
      * </p>
      *
      * @param n the positive integer number of steps to get to
@@ -71,8 +71,8 @@ public class ClimbingStairs {
      * Recursive, with memoization: find the number of distinct ways to
      * get to exactly n steps with steps of 1 and/or 2
      * <p>
-     *     O(n) time (size of recursion tree can go up to n)
-     *     O(n) space (depth of recursion tree can go up to n)
+     * O(n) time (size of recursion tree can go up to n)
+     * O(n) space (depth of recursion tree can go up to n)
      * </p>
      *
      * @param n the positive integer number of steps to get to
@@ -86,8 +86,8 @@ public class ClimbingStairs {
     /**
      * Recursive helper function for climbStairs2
      *
-     * @param i current step
-     * @param n destination step
+     * @param i    current step
+     * @param n    destination step
      * @param memo integer array storing the number of distinct ways to get to each i
      * @return number of distinct ways to arrange steps of 1 and/or 2 to get to n, starting from i
      */
@@ -105,5 +105,40 @@ public class ClimbingStairs {
         // never been to this i before, calculate and memoize
         memo[i] = climbStairs2helper(i + 1, n, memo) + climbStairs2helper(i + 2, n, memo);
         return memo[i];
+    }
+
+    /**
+     * Dynamic programming solution.
+     * <p>
+     * As we can see this problem can be broken into subproblems, and it contains the optimal
+     * substructure property i.e. its optimal solution can be constructed efficiently from optimal
+     * solutions of its subproblems, we can use dynamic programming to solve this problem.
+     * One can reach ith step in one of the two ways:
+     * <ol>
+     *     <li>Taking a single step from the (i-1)th step.</li>
+     *     <li>Taking a step of 2 from the (i-2)th step.</li>
+     * </ol>
+     * So if dp[i] denotes the number of ways to reach the ith step,
+     * <br/>
+     * dp[i] = dp[i-1] + dp[i-2]
+     * <p>
+     *     O(n) time, O(n) space
+     * </p>
+     *
+     * @param n destination step
+     * @return number of distinct ways to arrange steps of 1 and/or 2 to get to n, starting from i
+     */
+    public static int climbStairs3(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        // there is one way to get to the 1st step (1), and 2 ways to get to the 2nd step (1+2, 2+1)
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
