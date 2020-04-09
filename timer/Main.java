67b8d925c3a8fe82a44bfa63afcc0ae31a608a6b
jonjau <jonjau3Q@gmail.com>
2020-04-09 20:02:36 +1000

    day 9: backspace string compare (java)

--- timer/Main.java
+++ timer/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("Now running " + repeats + " time(s):");
         for (int i = 0; i < repeats; i++) {
             long startTime = System.nanoTime();
-            tdc.wk2.MiddleOfLinkedList.run();
+            tdc.wk2.BackspaceStringCompare.run();
             long endTime = System.nanoTime();
             long elapsedTime = endTime - startTime;
             // 1 second = 1_000_000_000 nanoseconds
