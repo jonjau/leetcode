dc9310ede6fc9d50d4905dbe5535baeb7271ccc2
jonjau <jonjau3Q@gmail.com>
2020-04-20 21:31:17 +1000

    added tree toString() method

--- tiq/tree/TreeNode.java
+++ tiq/tree/TreeNode.java
@@ -10,4 +10,26 @@ public class TreeNode {
         this.left = null;
         this.right = null;
     }
+
+    /**
+     * https://stackoverflow.com/questions/5071040/java-convert-integer-to-string
+     */
+    public StringBuilder toString(StringBuilder prefix, boolean isTail, StringBuilder sb) {
+        if (right != null) {
+            right.toString(new StringBuilder().append(prefix)
+                    .append(isTail ? "│   " : "    "), false, sb);
+        }
+        sb.append(prefix).append(isTail ? "└── " : "┌── ")
+                .append(Integer.toString(this.val)).append("\n");
+        if (left != null) {
+            left.toString(new StringBuilder().append(prefix)
+                    .append(isTail ? "    " : "│   "), true, sb);
+        }
+        return sb;
+    }
+
+    @Override
+    public String toString() {
+        return this.toString(new StringBuilder(), true, new StringBuilder()).toString();
+    }
 }
