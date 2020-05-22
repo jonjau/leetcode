16aa0bf83542b40488fbf59742fa58a886d015d5
jonjau <jonjau3Q@gmail.com>
2020-05-22 20:54:43 +1000

    day 22: sort characters by frequency (java)

---
+++ tdc2/wk4/SortCharactersByFrequency.java
@@ -0,0 +1,88 @@
+package tdc2.wk4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Given a string, sort it in decreasing order based on the frequency of characters.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * "tree"
+ * <p>
+ * Output:
+ * "eert"
+ * <p>
+ * Explanation:
+ * 'e' appears twice while 'r' and 't' both appear once.
+ * So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input:
+ * "cccaaa"
+ * <p>
+ * Output:
+ * "cccaaa"
+ * <p>
+ * Explanation:
+ * Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+ * Note that "cacaca" is incorrect, as the same characters must be together.
+ * <p>
+ * Example 3:
+ * <p>
+ * Input:
+ * "Aabb"
+ * <p>
+ * Output:
+ * "bbAa"
+ * <p>
+ * Explanation:
+ * "bbaA" is also a valid answer, but "Aabb" is incorrect.
+ * Note that 'A' and 'a' are treated as two different characters.
+ */
+public class SortCharactersByFrequency {
+    public static String run() {
+        String s = "Aabb";
+        String ans =sortCharactersByFrequency1(s);
+        System.out.println(ans);
+        return ans;
+    }
+
+    /**
+     * https://leetcode.com/problems/sort-characters-by-frequency/discuss/93445/O(n)-Easy-to-understand-Java-Solution
+     * <p>
+     * O(n) time, O(n) space
+     * </p>
+     *
+     * @param s a String
+     * @return the string sorted in decreasing order based on the frequency of characters
+     */
+    public static String sortCharactersByFrequency1(String s) {
+        if(s.length() < 3)
+            return s;
+        int max = 0;
+        int[] map = new int[256];
+        for(char ch : s.toCharArray()) {
+            map[ch]++;
+            max = Math.max(max,map[ch]);
+        }
+        String[] buckets = new String[max + 1]; // create max buckets
+        for(int i = 0 ; i < 256; i++) { // join chars in the same bucket
+            String str = buckets[map[i]];
+            if(map[i] > 0)
+                buckets[map[i]] = (str == null) ? "" + (char)i : (str + (char) i);
+        }
+        StringBuilder strb = new StringBuilder();
+        for(int i = max; i >= 0; i--) { // create string for each bucket.
+            if(buckets[i] != null)
+                for(char ch : buckets[i].toCharArray())
+                    for(int j = 0; j < i; j++)
+                        strb.append(ch);
+        }
+        return strb.toString();
+    }
+}
