ca32ae3f52367aa44301b7fccb75586a443f3fba
jonjau <jonjau3Q@gmail.com>
2020-10-14 00:44:19 +1100

    missing number (kotlin)

--- timer/Main.kt
+++ timer/Main.kt
@@ -4,7 +4,7 @@ import kotlin.system.measureNanoTime
 
 fun main() {
     val executionTime = measureNanoTime {
-        tiq.math.runFizzBuzz()
+        tiq.other.runMissingNumber()
     }
     val seconds = executionTime / 1_000_000_000
     println("--- $seconds seconds ---")
